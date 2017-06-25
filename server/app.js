c9495df1612845733515c2fd8ebfcab4b03abdae
Julian Craigwell-Graham <julian@julians-mbp.attlocal.net>
2017-06-25 18:55:54 -0500

    http post request saving to db

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ var app = express();
 var server = require('http').createServer(app);
 var path = require('path');
 
+//get parameters from body
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
 //make public folder static so that it can be accessed public
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -18,6 +22,9 @@ app.set('view engine', 'hbs');
 //connect to database
 require('./db/db.js');
 
+//connect to the model
+var User = require('./models/User.js');
+
 var database = [
 	{name: "Justin Graham",index: 0, title: "Consultant", location: "Chicago", image: "", topVenues: ["Bar Deville", "Slipper Slope", "East Room"], friends: [{name: "Julian", index: 1}, {name: "Jeannetta", index: 2}], loggedIn: true, goingOut: true},
 	{name: "Julian Graham",index: 1, title: "Student", location: "Chicago", image: "", topVenues: ["Beauty Bar", "Bar Deville", "Slipper Slope"], friends: [{name: "Justin", index: 0}, {name: "Jeannetta", index: 2}],loggedIn: true, goingOut: true},
@@ -26,7 +33,15 @@ var database = [
 
 	]
 
+// app.get('/', function(req, res){
+// 	User.find(function(err, wines){
 
+// 		var allWines = {wines: wines};
+// 		console.log(allWines);
+// 		res.render('home', allWines);
+// 	})
+	
+// })
 
 app.get('/home', function(req, res){
 
@@ -47,6 +62,25 @@ app.get('/profile', function(req, res){
 app.get('/join', function(req, res){
 
 	res.render('join');
+})
+
+app.post('/join', function(req, res){
+
+	console.log(req.body);
+	var user = new User({
+		name: req.body.name,
+		title: req.body.title,
+		location: req.body.location,
+		image: req.body.image,
+		venues: req.body.venues,
+		friends: req.body.friends,
+		logged: req.body.logged,
+		going: req.body.going
+
+	})
+
+	user.save();
+	res.send('success');
 })
 
 
---
+++ server/models/User.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var UserSchema = new mongoose.Schema({
+
+	name: String,
+	title: String,
+	location: String,
+	image: String,
+	venues: Array,
+	friends: Array,
+	logged: Boolean,
+	going: Boolean
+
+})
+
+var userModel = mongoose.model('User', UserSchema);
+
+module.exports = userModel;
+
+	// {name: "Justin Graham",index: 0, title: "Consultant", location: "Chicago", image: "",
+	//  topVenues: ["Bar Deville", "Slipper Slope", "East Room"], friends: [{name: "Julian", index: 1},
+	//   {name: "Jeannetta", index: 2}], loggedIn: true, goingOut: true},
